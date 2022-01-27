b78da849e122bf128f4a4444a377b481c43110e9
kangyonghyun <hmoon826@gmail.com>
2022-01-27 13:40:26 +0900

    (refactor) 역할을 분명하게 리팩토링

--- src/main/java/ladder/AppConfig.java
+++ src/main/java/ladder/AppConfig.java
@@ -1,12 +1,18 @@
 package ladder;
 
+import ladder.domain.DefaultLadderCreator;
+import ladder.domain.DefaultLineCreator;
 import ladder.domain.LadderCreator;
 import ladder.domain.LineCreator;
 
 public class AppConfig {
 
     public LadderCreator ladderCreator() {
-        return new LadderCreator(new LineCreator());
+        return new DefaultLadderCreator(lineCreator());
+    }
+
+    public LineCreator lineCreator() {
+        return new DefaultLineCreator();
     }
 
 }
