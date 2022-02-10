81ba706c3258ea891933c3e7ce4f3605b17d11bd
kangyonghyun <hmoon826@gmail.com>
2022-02-10 14:20:41 +0900

    (refactor) equals & hashcode 재정의

--- src/main/java/racingcar/domain/Winners.java
+++ src/main/java/racingcar/domain/Winners.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Winners {
 
@@ -21,6 +22,23 @@ public class Winners {
 
     public List<Car> getWinners() {
         return winners;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Winners winners1 = (Winners) o;
+        return Objects.equals(winners, winners1.winners);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(winners);
     }
 
 }
