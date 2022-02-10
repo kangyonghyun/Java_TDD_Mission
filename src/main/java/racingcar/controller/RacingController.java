d207a5d60180c1411801ea4a78566b33740f61b3
kangyonghyun <hmoon826@gmail.com>
2022-02-10 14:14:13 +0900

    (feat) Controller 생성

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,36 @@
+package racingcar.controller;
+
+import racingcar.domain.Cars;
+import racingcar.domain.RacingGame;
+import racingcar.domain.strategy.MovingStrategy;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+
+    private final MovingStrategy strategy;
+
+    public RacingController(MovingStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void play() {
+        // 자동차 이름 입력
+        Cars cars = Cars.of(InputView.inputCarName());
+        // 시도 횟수 입력
+        int tryNo = InputView.inputTryCount();
+
+        RacingGame racingGame = new RacingGame(cars, tryNo, strategy);
+
+        // 겜 시작
+        OutputView.printResultMessage();
+        while (!racingGame.isEndGame()) {
+            racingGame = racingGame.race();
+            OutputView.printResult(racingGame.getCars());
+        }
+
+        // 우승자 출력
+        OutputView.printWinners(racingGame.findWinners());
+    }
+
+}
