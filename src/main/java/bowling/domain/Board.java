ae4e05be66e67b26ac703dcf7ab9488e78e46a8a
kangyonghyun <hmoon826@gmail.com>
2022-02-18 17:11:04 +0900

    (refactor) equals & hashcode 추가

--- src/main/java/bowling/domain/Board.java
+++ src/main/java/bowling/domain/Board.java
@@ -3,6 +3,7 @@ package bowling.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Board {
 
@@ -15,17 +16,34 @@ public class Board {
 
     public void add(FrameResult frameResult) {
         if (!frameResult.isUnScore()) {
-            this.gameScore = frameResult.addTotalScore(gameScore);
+            this.gameScore = frameResult.addFrameScore(gameScore);
         }
         this.results.add(frameResult);
+    }
+
+    public List<FrameResult> getResults() {
+        return Collections.unmodifiableList(this.results);
     }
 
     public int getGameScore() {
         return this.gameScore;
     }
 
-    public List<FrameResult> getResults() {
-        return Collections.unmodifiableList(this.results);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Board board = (Board) o;
+        return gameScore == board.gameScore && Objects.equals(results, board.results);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(results, gameScore);
     }
 
     @Override
